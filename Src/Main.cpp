bcaad5a5ff779fdf7668381a156b035af765f2a5
thumurayusaku <49930447+thumurayusaku@users.noreply.github.com>
2019-06-03 16:54:03 +0900

    Add files via upload

---
+++ Src/Main.cpp
@@ -0,0 +1,19 @@
+#include "TitleScene.h"
+#include "GLFWEW.h"
+
+int main()
+{
+	GLFWEW::Window& window = GLFWEW::Window::Instance();
+	window.Init(1280, 720, u8"アクションゲーム");
+	SceneStack & sceneStack = SceneStack::Instance();
+	sceneStack.Push(std::make_shared<TitleScene>());
+
+	while (!window.ShouoldClose()) {
+		const float deltaTime = window.DeltaTime();
+		window.UpdateTimer();
+		sceneStack.Update(deltaTime);
+		sceneStack.Render();
+		window.SwapBuffers();
+
+	}
+}
---
+++ Src/TitleScene.cpp
@@ -0,0 +1,19 @@
+/**
+* @file TitleScene.cpp
+*/
+#include "TitleScene.h"
+#include "MainGameScene.h"
+#include "GLFWEW.h"
+
+
+
+/**
+* ƒvƒŒƒCƒ„[‚Ì“ü—Í‚ğˆ—‚·‚é.
+*/
+void TitleScene::ProcessInput()
+{
+	GLFWEW::Window& window = GLFWEW::Window::Instance();
+	if (window.GetGamePad().buttonDown & GamePad::START) {
+		SceneStack::Instance().Replace(std::make_shared<MainGameScene>());
+	}
+}
