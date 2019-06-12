1a2ac6e61bba14c242e768126034471d948c50ee
Yusaku thumura <tumt0823@icloud.com>
2019-06-12 10:52:02 +0900

    画像変更

--- Src/test.cpp
+++ Src/test.cpp
@@ -5,6 +5,8 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 	DxLib_Init();   // DXライブラリ初期化処理
 
+	LoadGraphScreen(0, 0, "title.jpg", TRUE); // 画像を描画する
+
 
 	WaitKey();      // キー入力待ち
 	DxLib_End();    // DXライブラリ終了処理
