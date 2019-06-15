34c280421789d0c4ac4814b32325ed6745cf9ee4
Yusaku thumura <tumt0823@icloud.com>
2019-06-15 12:44:47 +0900

    //

--- Src/main.cpp
+++ Src/main.cpp
@@ -2,17 +2,16 @@
 
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
+	ChangeWindowMode(FALSE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
 
 	SceneMgr_Initialize();
-
 
 	int Handle, Count = 0;
 	Handle = LoadSoundMem("Res/073.mp3"); // サウンド/1up.wavをロードし、識別番号をHandleに格納
 
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {//画面更新 & メッセージ処理 & 画面消去
 
-		
+
 		SceneMgr_Update();  //更新
 		SceneMgr_Draw();    //描画
 		if (Count % 120 == 0) { // 2秒に一度
@@ -21,6 +20,7 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 		Count++;
 
 	}
+
 
 	SceneMgr_Finalize();
 
