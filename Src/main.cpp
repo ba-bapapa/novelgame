d8640a78ea6175dcdf32075d129ab9223517c94a
Yusaku thumura <tumt0823@icloud.com>
2019-06-18 00:01:03 +0900

    クリック追加

--- Src/main.cpp
+++ Src/main.cpp
@@ -1,16 +1,23 @@
-#include "DxLib.h"
-#include "SceneMgr.h"
+#include "Include.h"
+
 
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 	ChangeWindowMode(TRUE), DxLib_Init(), SetDrawScreen(DX_SCREEN_BACK); //ウィンドウモード変更と初期化と裏画面設定
 
 	SceneMgr_Initialize();
 	double size = 1.0;
-	int x, y, Mouse, Wheel,Image;
+	int Handle, Count = 0;
+	Handle = LoadSoundMem("Res\\073.mp3"); // サウンド/1up.wavをロードし、識別番号をHandleに格納
+	int x, y, Mouse, Wheel, Image;
 
 	Image = LoadGraph("Res\\A.png");//画像ロード
 
 	while (ScreenFlip() == 0 && ProcessMessage() == 0 && ClearDrawScreen() == 0) {//画面更新 & メッセージ処理 & 画面消去
+
+		if (Count % 120 == 0) { // 2秒に一度
+			PlaySoundMem(Handle, DX_PLAYTYPE_BACK); // 効果音を再生する
+		}
+		Count++;
 
 		Mouse = GetMouseInput();                //マウスの入力状態取得
 		Wheel = GetMouseWheelRotVol();  //ホイールの回転量取得
